f26bcfe865f4a977ad024f95096f92bf1b927783
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-04-04 16:35:02 +0000

    updaet

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 // Copyright (c) 2023 Osamah Hasan All rights reserved
 //
 // Created by: Osamah Hasan
-// Created on: Feb 2023
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
 
-function myButtonClicked() {
-  alert("Answer will be supported soon")
+'use strict'
+/**
+ * This function calculates area and perimeter of Triangle.
+ */
+function calculate () {
+  // input
+  const L = parseInt(document.getElementById('L of Pyramid').value)
+  const H = parseInt(document.getElementById('H of Pyramid').value)
+  const W = parseInt(document.getElementById('W of Pyramid').value)
+
+  // process
+  const Volume = (L * H * W) / 3
+
+  // output
+  document.getElementById('Volume').innerHTML = 'Volume is: ' + Volume + ' cm²'
 }
